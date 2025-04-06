9eacde4c59580def222c72f7ecdac44197a66e8c
pramod kumar <pramod.nkumar@collabera.com>
2025-04-06 19:04:17 +0530

    added new project

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/bye/{value}")
+    public String bye(@PathVariable("value") String value) {
+        return value;
+    }
+
+    @GetMapping("/foos")
+    @ResponseBody
+    public String getFoos(@RequestParam String id) {
+        return "ID: " + id;
+    }
+    @PostMapping("/foos")
+    @ResponseBody
+    public String addFoo(@RequestParam String fooId, @RequestParam Optional <String> name) {
+        return "ID: " + fooId + " Name: " + name.orElseGet(() -> "not provided");
+    }
+}
